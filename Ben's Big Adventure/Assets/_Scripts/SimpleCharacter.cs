0a12d9be79ce664776ed83e7e55e89ca2420db22
mjtodd95 <mjtodd95@gmail.com>
2017-08-02 17:07:44 +0100

    Wall and double jumping added :)

--- Ben's Big Adventure/Assets/_Scripts/SimpleCharacter.cs
+++ Ben's Big Adventure/Assets/_Scripts/SimpleCharacter.cs
@@ -9,56 +9,94 @@ public class SimpleCharacter : MonoBehaviour {
     public Transform cameraTransform;
     public float speed = 3f;
     public float gravityStrength = 9.8f;
-    public float jumpSpeed = 5f;
-    bool canJump = false;
+    public float jumpSpeed = 3f;
+    bool canDoubleJump = false;
     float verticalVelocity;
+    Vector3 velocity;
+    Vector3 groundedVelocity;
+    Vector3 normal;
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         //Sets up character postion
         Vector3 myVector = new Vector3(0, 0, 0);
 
-        //get input from player
-        myVector.x = Input.GetAxis("Horizontal");
-        myVector.z = Input.GetAxis("Vertical");
-        myVector = Vector3.ClampMagnitude(myVector, 1f);
+        if (myController.isGrounded)
+        {
+            //get input from player
+            myVector.x = Input.GetAxis("Horizontal");
+            myVector.z = Input.GetAxis("Vertical");
+            myVector = Vector3.ClampMagnitude(myVector, 1f);
 
-        myVector = myVector * speed * Time.deltaTime;
+            myVector = myVector * speed * Time.deltaTime;
 
-        //rotate input by direction of camera
-        //keeps input relative to camera
-        Quaternion inputRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(cameraTransform.forward, Vector3.up));
-        myVector = inputRotation * myVector;
+            //rotate input by direction of camera
+            //keeps input relative to camera
+            Quaternion inputRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(cameraTransform.forward, Vector3.up));
+            myVector = inputRotation * myVector;
+
+        }
+        else
+        {
+            myVector = groundedVelocity;
+            myVector *= Time.deltaTime;
+        }
 
         //Adjusts vertical velocity according to gravity
-        verticalVelocity = verticalVelocity - gravityStrength*Time.deltaTime;
+        verticalVelocity = verticalVelocity - gravityStrength * Time.deltaTime;
+
+        //set newly calculated vertical velocity
+        myVector.y = verticalVelocity * Time.deltaTime;
+
+        //Use input to move player
+        CollisionFlags flags = myController.Move(myVector);
+        velocity = myVector / Time.deltaTime;
+        
 
         //Enables character to jump
         if (Input.GetButtonDown("Jump"))
         {
-            if (canJump)
+
+            //enables double jump
+            if (canDoubleJump)
             {
+                //increase vertical velocity
+                verticalVelocity += (jumpSpeed / 2);
+
+                //ensures no more than 2 jumps may occur
+                canDoubleJump = false;
+            }
+            if ((flags & CollisionFlags.Below) != 0)
+            {
+                //set grounded velocity
+                groundedVelocity = Vector3.ProjectOnPlane(velocity, Vector3.up);
+
+                //smooth movement down ramps
+                verticalVelocity = -1f;
+
+                //increase vertical velocity
+                verticalVelocity += jumpSpeed;
+
+                //enables a second double jump
+                canDoubleJump = true;
+            }
+            if ( ( ((flags & CollisionFlags.Sides) != 0) & (flags & CollisionFlags.Below) == 0) ) 
+            {
+                groundedVelocity = Vector3.Reflect(groundedVelocity, normal);
+                print("reached");
+                //increase vertical velocity
                 verticalVelocity += jumpSpeed;
             }
         }
 
-        //adjust character postion according to new calculated velocity
-        //jumping/ gravity etc
-        myVector.y = verticalVelocity*Time.deltaTime;
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        normal = hit.normal;
+    }
 
-        //Use input to move player
-        CollisionFlags flags = myController.Move(myVector);
 
-        //if in contact with ground
-        if((flags & (CollisionFlags.Sides|CollisionFlags.Below)) != 0)
-        {
-            canJump = true;
-            verticalVelocity = -1f;
-        }
-        else
-        {
-            canJump = false;
-        }
-
-	}
 }
+    
+
