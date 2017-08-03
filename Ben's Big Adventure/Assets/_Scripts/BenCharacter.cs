e7a4d32d6359bbf5301a200b799fd20dbc18920a
mjtodd95 <mjtodd95@gmail.com>
2017-08-03 16:45:35 +0100

    Added 3rd person camera following + a start on using 3d models

---
+++ Ben's Big Adventure/Assets/_Scripts/BenCharacter.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BenCharacter : MonoBehaviour
+{
+
+    //Capsule character
+    public CharacterController myController;
+    public Transform cameraTransform;
+    public float speed = 3f;
+    public float gravityStrength = 9.8f;
+    public float jumpSpeed = 4f;
+    bool canDoubleJump = false;
+    float verticalVelocity;
+    Vector3 velocity;
+    Vector3 groundedVelocity;
+    Vector3 normal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets up character postion
+        Vector3 myVector = new Vector3(0, 0, 0);
+
+        if (myController.isGrounded)
+        {
+            //get input from player
+            myVector.x = Input.GetAxis("Horizontal");
+            myVector.z = Input.GetAxis("Vertical");
+            myVector = Vector3.ClampMagnitude(myVector, 1f);
+
+            myVector = myVector * speed * Time.deltaTime;
+
+            //rotate input by direction of camera
+            //keeps input relative to camera
+            Quaternion inputRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(cameraTransform.forward, Vector3.up));
+            myVector = inputRotation * myVector;
+
+        }
+        else
+        {
+            myVector = groundedVelocity;
+            myVector *= Time.deltaTime;
+        }
+
+        //Adjusts vertical velocity according to gravity
+        verticalVelocity = verticalVelocity - gravityStrength * Time.deltaTime;
+
+        //set newly calculated vertical velocity
+        myVector.y = verticalVelocity * Time.deltaTime;
+
+        //Use input to move player
+        CollisionFlags flags = myController.Move(myVector);
+        velocity = myVector / Time.deltaTime;
+
+
+        //Enables character to jump
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            //enables double jump
+            if (canDoubleJump)
+            {
+                //increase vertical velocity
+                verticalVelocity += (jumpSpeed / 2);
+
+                //ensures no more than 2 jumps may occur
+                canDoubleJump = false;
+            }
+            if ((flags & CollisionFlags.Below) != 0)
+            {
+                //set grounded velocity
+                groundedVelocity = Vector3.ProjectOnPlane(velocity, Vector3.up);
+
+                //smooth movement down ramps
+                verticalVelocity = -1f;
+
+                //increase vertical velocity
+                verticalVelocity += jumpSpeed;
+
+                //enables a second double jump
+                canDoubleJump = true;
+            }
+            if ((((flags & CollisionFlags.Sides) != 0) & (flags & CollisionFlags.Below) == 0))
+            {
+                groundedVelocity = Vector3.Reflect(groundedVelocity, normal);
+                print("reached");
+                //increase vertical velocity
+                verticalVelocity += jumpSpeed;
+            }
+        }
+
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        normal = hit.normal;
+    }
+
+
+}
---
+++ Ben's Big Adventure/Assets/_Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = -50.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 5.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 8.0f;
+    private float sensitivityY = 1.0f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            distance += 0.2f;
+
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            distance -= 0.2f;
+
+        }
+
+        currentX += Input.GetAxis("Mouse X");
+        currentY -= Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+
+        camTransform.LookAt(lookAt.position);
+    }
+
+}
